58631f4b8c56cab5d95a19260565ef19505c3621
Midshar <midshar@yeah.net>
2025-10-24 21:02:27 +0800

    --fix:修改配置

--- vite.config.js
+++ vite.config.js
@@ -31,9 +31,25 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        // manualChunks: undefined, // 禁用代码分割
-        dir:'docs'
-      },
-    },
-  },
+        dir:'docs',
+        // 控制 JS 文件（包括入口文件和动态导入的块）
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+
+        // 控制静态资源（CSS、图片、字体等）
+        assetFileNames: ({ name }) => {
+          if (/\.css$/.test(name)) {
+            return 'css/[name].[ext]' // CSS 文件放到 css/ 目录
+          }
+          if (/\.(png|gif|svg|ico|woff2?|eot|ttf|otf)$/.test(name)) {
+            return 'other/[name].[ext]' // 其他静态资源放到 other/ 目录
+          }
+          if (/\.(jpg|jpeg|webp)$/.test(name)) {
+            return 'assets/static/imgs/[name].[ext]'
+          }
+          return '[name].[ext]' // 默认处理（理论上不会用到）
+        }
+      }
+    }
+  }
 })
